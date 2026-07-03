732c6fd3c81269bd4c7e669cfb2e5fca8225fc21
vanderbake <33555008+vanderbake@users.noreply.github.com>
2026-07-03 15:27:14 +0200

    Add package service provider

---
+++ src/FilamentTrustbirdServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Trustbird\Filament;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+final class FilamentTrustbirdServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('filament-trustbird')
+            ->hasConfigFile()
+            ->hasViews();
+    }
+}
